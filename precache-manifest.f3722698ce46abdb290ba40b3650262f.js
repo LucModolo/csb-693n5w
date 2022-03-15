5b7388f049443d1895353c6debce37ed2dd9fda2
Code Sandbox <hello@codesandbox.io>
2022-03-15 15:38:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f3722698ce46abdb290ba40b3650262f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9477e605cd35f48de54",
+    "url": "https://lucmodolo.github.io/csb-693n5w/static/css/main.eaa1648d.chunk.css"
+  },
+  {
+    "revision": "b9477e605cd35f48de54",
+    "url": "https://lucmodolo.github.io/csb-693n5w/static/js/main.b9477e60.chunk.js"
+  },
+  {
+    "revision": "bed57b426dc80c1bb7cd",
+    "url": "https://lucmodolo.github.io/csb-693n5w/static/css/1.ed49ba9e.chunk.css"
+  },
+  {
+    "revision": "bed57b426dc80c1bb7cd",
+    "url": "https://lucmodolo.github.io/csb-693n5w/static/js/1.bed57b42.chunk.js"
+  },
+  {
+    "revision": "eca7c8e503c99915eb54",
+    "url": "https://lucmodolo.github.io/csb-693n5w/static/js/runtime~main.eca7c8e5.js"
+  },
+  {
+    "revision": "d06be0b5e7e8d95f3e461008ce7b9317",
+    "url": "https://lucmodolo.github.io/csb-693n5w/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://lucmodolo.github.io/csb-693n5w/precache-manifest.f3722698ce46abdb290ba40b3650262f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://lucmodolo.github.io/csb-693n5w/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
